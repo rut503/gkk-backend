d2b6338de19b4b5204dbcf5291b4ef735c9de858
Greg <Greggomez11235@gmail.com>
2021-11-19 11:49:52 -0600

    Implemented put operation

--- app/routes/review_for_consumer_routes.py
+++ app/routes/review_for_consumer_routes.py
@@ -2,9 +2,11 @@ from typing import Optional, List
 from fastapi import APIRouter, HTTPException
 from bson import ObjectId
 from fastapi.params import Query
+from pymongo.message import update
 from models.review_for_consumer_model import *
 from config.database import review_for_consumer_collection
 from schemas.review_for_consumer_schema import review_for_consumer_serializer, reviews_for_consumer_serializer
+from datetime import datetime
 
 review_for_consumer_router = APIRouter()
 
@@ -57,7 +59,7 @@ async def get_review_for_consumer_by_id(id: str):
 
     return serialized_review_document
 
-# Post a review
+# Post a single review
 @review_for_consumer_router.post("/", response_model=review_for_consumer_response)
 async def post_review_for_consumer(review: review_for_consumer_post):
     review_to_insert = {"date_created" : datetime.today(), "date_updated" : datetime.today(), **review.dict()}
@@ -66,14 +68,32 @@ async def post_review_for_consumer(review: review_for_consumer_post):
     if not result.inserted_id:
         raise HTTPException(status_code=400, detail="Error while inserting")
 
-    # This does an extra query for every insert
-    # inserted_review = review_for_consumer_collection.find_one({"_id": result.inserted_id})
-    # inserted_review = review_for_consumer_serializer(inserted_review)
+    # Retrieving added document
+    inserted_review = review_for_consumer_collection.find_one({"_id": result.inserted_id})
+    inserted_review = review_for_consumer_serializer(inserted_review)
 
-    #print(type(result.inserted_id.__str__()))
-    #return inserted_review
+    return inserted_review
 
-    # This way doesn't use a query
-    review_to_insert["id"] = result.inserted_id.__str__()
+#@review_for_consumer_router.put("/id/{id}", respose_model=review_for_consumer_response)
+@review_for_consumer_router.put("/id/{id}")
+async def update_review_for_consumer(review: review_for_consumer_put, id: str):
+    review_to_update_dict = review_for_consumer_serializer(review_for_consumer_collection.find_one({"_id": ObjectId(id)}))
+    review_to_update_dict ['date_updated'] = datetime.today()
+    review_identifier = review_to_update_dict['id']
 
-    return review_to_insert
+    fields_to_change_put_model = review_for_consumer_put(**review_to_update_dict)
+    updated_data_dict = review.dict(exclude_unset=True)
+    updated_put_model = fields_to_change_put_model.copy(update=updated_data_dict)
+    # review_to_update['rating'] = review_for_consumer_put['rating']
+    # review_to_update['title'] = review_for_consumer_put['title']
+    # review_to_update['description'] = review_for_consumer_put['description']  
+
+    review_for_consumer_collection.update_one({"_id": ObjectId(review_identifier)}, {'$set': updated_put_model.dict()})
+    #update_data = review.dict(exclude_unset=True)
+    #update_item = reivew_to_update.copy(update=update_data)
+
+    #print(review_to_update)
+
+    #updated_item['id'] = review_identifier
+    return updated_put_model
+
