1cd870d9fcf913dc432588838055309475a425d2
Greg <Greggomez11235@gmail.com>
2021-12-03 16:15:36 -0600

    Refactored get call. Now uses a Query parameter, field, to limit which fields are returned.

--- app/routes/producer_routes.py
+++ app/routes/producer_routes.py
@@ -13,18 +13,65 @@ from schemas.producer_schema import producer_serializer
 producer_router = APIRouter()
 
 # Get a producer by id
+# @producer_router.get("/{id}", response_model=producer_response, response_model_exclude_none=True)
+# async def get_producer_by_id(id: str = Path(..., min_length=24, max_length=24),
+#                              first_name: bool = True,
+#                              last_name: bool = True,
+#                              phone_number: bool = True,
+#                              address: bool = True,
+#                              food_items: bool = True,
+#                              rating: bool = True,
+#                              active_orders: bool = True,
+#                              menu: bool = True,
+#                              date_created: bool = True,
+#                              date_updated: bool = True):
+    
+#     if not ObjectId.is_valid(id):
+#         raise HTTPException(
+#              status_code=status.HTTP_400_BAD_REQUEST,
+#              detail=id + " is not a valid ObjectId type!"
+#         )
+    
+#     # Creating a filter_fields model with fields set to the query parameters
+#     field_to_filter = filter_fields()
+#     field_to_filter.__setattr__('first_name', first_name)
+#     field_to_filter.__setattr__('last_name', last_name)
+#     field_to_filter.__setattr__('phone_number', phone_number)
+#     field_to_filter.__setattr__('address', address)
+#     field_to_filter.__setattr__('food_items', food_items)
+#     field_to_filter.__setattr__('rating', rating)
+#     field_to_filter.__setattr__('active_orders', active_orders)
+#     field_to_filter.__setattr__('menu', menu)
+#     field_to_filter.__setattr__('date_created', date_created)
+#     field_to_filter.__setattr__('date_updated', date_updated)
+    
+#     # Creates a field_to_filter model 
+#     fields_to_filer_dict = field_to_filter.dict(exclude_defaults=True) 
+
+#     # Checks if any query parameters were sent in
+#     if len(fields_to_filer_dict) != 0: 
+#         producer_dict = producer_collection.find_one({"_id": ObjectId(id)}, fields_to_filer_dict)
+#         if producer_dict is None:
+#             raise HTTPException(
+#                 status_code=404,
+#                 detail="Producer not found with id " + id
+#             )
+
+#         return producer_serializer(producer_dict)
+
+#     else:
+#         producer_dict = producer_collection.find_one({"_id": ObjectId(id)})
+#         if producer_dict is None:
+#             raise HTTPException(
+#                 status_code=404,
+#                 detail="Producer not found with id " + id
+#             )
+        
+#         return producer_serializer(producer_dict)
+
 @producer_router.get("/{id}", response_model=producer_response, response_model_exclude_none=True)
 async def get_producer_by_id(id: str = Path(..., min_length=24, max_length=24),
-                             first_name: bool = True,
-                             last_name: bool = True,
-                             phone_number: bool = True,
-                             address: bool = True,
-                             food_items: bool = True,
-                             rating: bool = True,
-                             active_orders: bool = True,
-                             menu: bool = True,
-                             date_created: bool = True,
-                             date_updated: bool = True):
+                             fields: List[str] = Query(None)):
     
     if not ObjectId.is_valid(id):
         raise HTTPException(
@@ -32,42 +79,21 @@ async def get_producer_by_id(id: str = Path(..., min_length=24, max_length=24),
              detail=id + " is not a valid ObjectId type!"
         )
     
-    # Creating a filter_fields model with fields set to the query parameters
-    field_to_filter = filter_fields()
-    field_to_filter.__setattr__('first_name', first_name)
-    field_to_filter.__setattr__('last_name', last_name)
-    field_to_filter.__setattr__('phone_number', phone_number)
-    field_to_filter.__setattr__('address', address)
-    field_to_filter.__setattr__('food_items', food_items)
-    field_to_filter.__setattr__('rating', rating)
-    field_to_filter.__setattr__('active_orders', active_orders)
-    field_to_filter.__setattr__('menu', menu)
-    field_to_filter.__setattr__('date_created', date_created)
-    field_to_filter.__setattr__('date_updated', date_updated)
-    
-    # Creates a field_to_filter model 
-    fields_to_filer_dict = field_to_filter.dict(exclude_defaults=True) 
-
-    # Checks if any query parameters were sent in
-    if len(fields_to_filer_dict) != 0: 
-        producer_dict = producer_collection.find_one({"_id": ObjectId(id)}, fields_to_filer_dict)
-        if producer_dict is None:
-            raise HTTPException(
-                status_code=404,
-                detail="Producer not found with id " + id
-            )
-
-        return producer_serializer(producer_dict)
-
+    if fields is not None:
+        fields_to_return = {key: 1 for key in fields}
+        producer_dict = producer_collection.find_one({"_id": ObjectId(id)}, fields_to_return)
     else:
         producer_dict = producer_collection.find_one({"_id": ObjectId(id)})
-        if producer_dict is None:
-            raise HTTPException(
-                status_code=404,
-                detail="Producer not found with id " + id
-            )
-        
-        return producer_serializer(producer_dict)
+    
+
+    # Checks if any query parameters were sent in    
+    if producer_dict is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Producer not found with id " + id
+        )
+
+    return producer_serializer(producer_dict)
 
 # Get call with phone number
 @producer_router.get("/phone_number/{phone_number_param}", response_model=producer_response, response_model_exclude_none=True)
