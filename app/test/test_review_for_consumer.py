62f4df31638f13039d3fad89e5c0dd13a3dc63b7
Greg <Greggomez11235@gmail.com>
2021-12-13 17:08:33 -0600

    Created 2 more tests for get and post operations

--- app/test/test_review_for_consumer.py
+++ app/test/test_review_for_consumer.py
@@ -14,12 +14,12 @@ from pytest import mark, raises
 from app.models.review_for_consumer_model import review_for_consumer_response
 client = TestClient(app)
 
-VALID_ID         = "61b6f11a41e7064d92e23641"
-UNAVAILABLE_ID   = "111111111111111111111111"
-INVALID_ID       = "xxxxxxxxxxxxxxxxxxxxxxxx"
-UNPROCESSABLE_ID = "1111111111111111111111111"
-CONSUMER_ID      = "61b6f10641e7064d92e2361e"
-PRODUCER_ID      = "61b6f10841e7064d92e23621"
+VALID_ID          = "61b6f11a41e7064d92e23641"
+UNAVAILABLE_ID    = "111111111111111111111111"
+INVALID_OBJECT_ID = "xxxxxxxxxxxxxxxxxxxxxxxx"
+TOO_LONG_ID       = "1111111111111111111111111"
+CONSUMER_ID       = "61b6f10641e7064d92e2361e"
+PRODUCER_ID       = "61b6f10841e7064d92e23621"
 
 class Test_Get_By_Id:
      # Successful call
@@ -34,25 +34,26 @@ class Test_Get_By_Id:
           assert response.status_code == 404
           assert raises(HTTPException)
 
-     # Unprocessible id param
-     def test_get_by_unprocessable_id(self):
-          response = client.get("/review_for_consumer/" + UNPROCESSABLE_ID)
+     # Too long id
+     def test_get_by_too_long_id(self):
+          response = client.get("/review_for_consumer/" + TOO_LONG_ID)
           assert response.status_code == 422
           assert raises(RequestValidationError)
 
-     # Invalid Id
+     # Invalid object Id
      def test_get_by_invalid_id(self):
-          response = client.get("/review_for_consumer/" + INVALID_ID)
+          response = client.get("/review_for_consumer/" + INVALID_OBJECT_ID)
           assert response.status_code == 400
           assert raises(HTTPException)
 
-     # Empty
+     # Empty id
      def test_get_by_empty_id(self):
           response = client.get("/review_for_consumer/")
           assert response.status_code == 405
           assert raises(WebSocketRequestValidationError)
 
 class Test_Get_By_Consumer_And_Producer:
+     #Successful 
      def test_get_by_valid_consumer_id_and_producer_id(self):
           response = client.get("/review_for_consumer", params={"consumer_id": CONSUMER_ID, "producer_id": PRODUCER_ID})
           assert response.status_code == 200
@@ -60,15 +61,6 @@ class Test_Get_By_Consumer_And_Producer:
           # Validating documents returned
           for document in response.json():
                response_model = review_for_consumer_response(**document)
-
-     # Valid id that returns an empty list
-     @mark.parametrize("producer_or_consumer",[
-          ("producer_id"),
-          ("consumer_id")
-     ])
-     def test_get_by_consumer_id_and_producer_id_no_reviews(self, producer_or_consumer):
-          response = client.get("/review_for_consumer", params={producer_or_consumer: VALID_ID})
-          assert response.status_code == 404 
 
      # Get reviews from a producer_id or a consumer_id
      @mark.parametrize("id, producer_or_consumer",[
@@ -83,8 +75,27 @@ class Test_Get_By_Consumer_And_Producer:
           # Validating documents returned
           for document in response.json():
                review_for_consumer_response(**document)
-          
-     # Field validation check
+
+     # Valid id that returns an empty list
+     @mark.parametrize("producer_or_consumer",[
+          ("producer_id"),
+          ("consumer_id")
+     ])
+     def test_get_by_consumer_id_and_producer_id_no_reviews(self, producer_or_consumer):
+          response = client.get("/review_for_consumer", params={producer_or_consumer: VALID_ID})
+          assert response.status_code == 404 
+
+     # Too long id
+     @mark.parametrize("producer_or_consumer, id",[
+          ("producer_id", TOO_LONG_ID),
+          ("consumer_id", TOO_LONG_ID)
+     ])
+     def test_get_by_consumer_id_and_producer_id_too_long(self, producer_or_consumer, id):
+          response = client.get("/review_for_consumer", params={producer_or_consumer: id})
+          assert raises(RequestValidationError)
+          assert response.status_code == 422
+
+     # Empty id
      @mark.parametrize("producer_id, consumer_id",[
           (PRODUCER_ID, ""),
           ("", CONSUMER_ID),
@@ -95,20 +106,21 @@ class Test_Get_By_Consumer_And_Producer:
           assert response.status_code == 422
           assert raises(RequestValidationError)
 
-     # Valid id check
+     # Invalid object id
      def test_get_by_consumer_id_and_producer_id_invalid_ids(self):
-          response = client.get("/review_for_consumer", params={"consumer_id": INVALID_ID, "producer_id": INVALID_ID})
+          response = client.get("/review_for_consumer", params={"consumer_id": INVALID_OBJECT_ID, "producer_id": INVALID_OBJECT_ID})
           assert response.status_code == 400
           assert raises(HTTPException)
 
 
-rating = 2
-title = "This is a test for posting a review for consumer"
-description = "This is the test description"
+RATING = 2
+TITLE = "This is a test for posting a review for consumer"
+DESCRIPTION = "This is the test description"
 
 class Test_Post_Review_For_Consumer:
-     def test_post(self):
-          payload = {"consumer_id": CONSUMER_ID, "producer_id": PRODUCER_ID, "rating": rating, "title": title, "description": description}
+     # Post with all fields passed
+     def test_post_valid_all_fields(self):
+          payload = {"consumer_id": CONSUMER_ID, "producer_id": PRODUCER_ID, "rating": RATING, "title": TITLE, "description": DESCRIPTION}
           response = client.post("review_for_consumer/", json=payload)
 
           response_status_code = response.status_code
@@ -118,36 +130,51 @@ class Test_Post_Review_For_Consumer:
           assert response_status_code == 201
           assert response["producer_id"] == PRODUCER_ID
           assert response["consumer_id"] == CONSUMER_ID
-          assert response["rating"] == rating
-          assert response["title"] == title
-          assert response["description"] == description
+          assert response["rating"] == RATING
+          assert response["title"] == TITLE
+          assert response["description"] == DESCRIPTION
      
+     # Only one id passed, need a producer AND consumer id for posting a review
      @mark.parametrize("id, producer_or_consumer",[
           (PRODUCER_ID, "producer_id"),
           (CONSUMER_ID, "consumer_id")
      ])
      def test_post_only_one_id(self, id, producer_or_consumer):
-          payload = {producer_or_consumer: id, "rating": rating, "title": title, "description": description}
+          payload = {producer_or_consumer: id, "rating": RATING, "title": TITLE, "description": DESCRIPTION}
           response = client.post("review_for_consumer/", json=payload)
           assert response.status_code == 422
           assert raises(RequestValidationError)
 
+          # Only one id passed, need a producer AND consumer id for posting a review
+     @mark.parametrize("producer_id, consumer_id",[
+          (INVALID_OBJECT_ID, CONSUMER_ID),
+          (PRODUCER_ID, INVALID_OBJECT_ID)
+     ])
+     def test_post_invalid_id(self, producer_id, consumer_id):
+          payload = {"producer_id": producer_id, "consumer_id": consumer_id, "rating": RATING, "title": TITLE, "description": DESCRIPTION}
+          response = client.post("review_for_consumer/", json=payload)
+          assert response.status_code == 400
+          assert raises(HTTPException)
+
+     # Post with empty id's
      @mark.parametrize("producer_id, consumer_id",[
           (PRODUCER_ID, ""),
           ("", CONSUMER_ID),
           ("", "")
      ])
-     def test_post_empty(self, producer_id, consumer_id):
-          payload = {"consumer_id": consumer_id, "producer_id": producer_id, "rating": rating, "title": title, "description": description}
+     def test_post_empty_ids(self, producer_id, consumer_id):
+          payload = {"consumer_id": consumer_id, "producer_id": producer_id, "rating": RATING, "title": TITLE, "description": DESCRIPTION}
           response = client.post("review_for_consumer/", json=payload)
-          assert raises(RequestValidationError)
           assert response.status_code == 422
-         
+          assert raises(RequestValidationError)
+
+
+
 # class Test_Review_for_Consumer:
 #      # Get operation via id. Validating status code
 #      @mark.parametrize("id, status, exception", [
 #      (valid_id, 200, None),
-#      (unprocessable_id, 422, RequestValidationError),
+#      (TOO_LONG_ID, 422, RequestValidationError),
 #      (invalid_id, 404, HTTPException)])
 #      def test_get_review_for_consumer_by_id(self, id, status, exception):
 #           response = client.get("/review_for_consumer/"+id)
