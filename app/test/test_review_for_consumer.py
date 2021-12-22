c8ee7f13843f5f198203e4cfdd75743665f79d39
Greg <Greggomez11235@gmail.com>
2021-12-21 20:59:04 -0600

    Refactored code

--- app/test/test_review_for_consumer.py
+++ app/test/test_review_for_consumer.py
@@ -17,17 +17,18 @@ INVALID_OBJECT_ID = "xxxxxxxxxxxxxxxxxxxxxxxx"
 TOO_LONG_ID       = "1111111111111111111111111"
 CONSUMER_ID       = "61b6f10641e7064d92e2361e"
 PRODUCER_ID       = "61b6f10841e7064d92e23621"
+POST_RATING = 2
+POST_TITLE = "This is a test for posting a review for consumer"
+POST_DESCRIPTION = "This is the test description"
 
 # Creates a test document and returns the new document. Deletes the document at the end. 
 @fixture
 def get_posted_review():   
-    payload = {"consumer_id": ObjectId(CONSUMER_ID), "producer_id": ObjectId(PRODUCER_ID), "rating": "2", "title": "This a test post", "description": "This is the test description", "date_created": datetime.utcnow(), "date_updated": datetime.utcnow()}
+    payload = {"consumer_id": ObjectId(CONSUMER_ID), "producer_id": ObjectId(PRODUCER_ID), "rating": POST_RATING, "title": POST_TITLE, "description": POST_DESCRIPTION, "date_created": datetime.utcnow(), "date_updated": datetime.utcnow()}
     post_id = review_for_consumer_collection.insert_one(payload).inserted_id
     post_doc = review_for_consumer_collection.find_one({"_id": post_id})
     post_doc["id"] = str(post_doc["_id"])
     
-    print(type(post_doc["producer_id"]))
-
     del post_doc["_id"]
     yield post_doc
     
@@ -45,14 +46,18 @@ class Test_Get_By_Id:
     # Successful call
     def test_get_by_id_in_db(self, get_posted_review):
         response = client.get("/review_for_consumer/" + get_posted_review["id"])
-        response_model = review_for_consumer_response(**response.json())
+        response_model = review_for_consumer_response(**response.json()).dict()
+        assert response_model["consumer_id"] == CONSUMER_ID
+        assert response_model["producer_id"] == PRODUCER_ID
+        assert response_model["rating"] == POST_RATING
+        assert response_model["title"] == POST_TITLE
+        assert response_model["description"] == POST_DESCRIPTION
         assert response.status_code == 200
 
     # Unavailable document
     def test_get_by_id_not_in_db(self):
         response = client.get("/review_for_consumer/" + UNAVAILABLE_ID)
         assert response.status_code == 404
-        assert raises(HTTPException)
 
     # Too long id
     def test_get_by_too_long_id(self):
@@ -63,7 +68,6 @@ class Test_Get_By_Id:
     def test_get_by_invalid_id(self):
         response = client.get("/review_for_consumer/" + INVALID_OBJECT_ID)
         assert response.status_code == 400
-        assert raises(HTTPException)
 
     # Empty id
     def test_get_by_empty_id(self):
@@ -71,14 +75,14 @@ class Test_Get_By_Id:
         assert response.status_code == 405
 
 class Test_Get_By_Consumer_And_Producer:
-    #Successful 
+    # Successful 
     def test_get_by_valid_consumer_id_and_producer_id(self, get_posted_review):
         response = client.get("/review_for_consumer", params={"consumer_id": get_posted_review["consumer_id"], "producer_id": get_posted_review["producer_id"]})
-        assert response.status_code == 200
         
         # Validating documents returned
         for document in response.json():
             response_model = review_for_consumer_response(**document)
+        assert response.status_code == 200
 
     # Get reviews from a producer_id or a consumer_id
     @mark.parametrize("id, producer_or_consumer",[
@@ -89,10 +93,10 @@ class Test_Get_By_Consumer_And_Producer:
         response = client.get("/review_for_consumer", params={producer_or_consumer: id})
         response_status_code = response.status_code
 
-        assert response_status_code == 200
         # Validating documents returned
         for document in response.json():
             review_for_consumer_response(**document)
+        assert response_status_code == 200
 
     # Valid id that returns an empty list
     @mark.parametrize("producer_or_consumer",[
@@ -103,7 +107,7 @@ class Test_Get_By_Consumer_And_Producer:
         response = client.get("/review_for_consumer", params={producer_or_consumer: VALID_ID})
         assert response.status_code == 404 
 
-    # Too long id
+    # Testing with an invalid ObjectId
     @mark.parametrize("producer_or_consumer, id",[
         ("producer_id", TOO_LONG_ID),
         ("consumer_id", TOO_LONG_ID)
@@ -112,7 +116,7 @@ class Test_Get_By_Consumer_And_Producer:
         response = client.get("/review_for_consumer", params={producer_or_consumer: id})
         assert response.status_code == 422
 
-    # Empty id
+    # Testing with empty id's passed in
     @mark.parametrize("producer_id, consumer_id",[
         (lazy_fixture('get_posted_producer_id'), ""),
         ("", lazy_fixture('get_posted_consumer_id')),
@@ -122,46 +126,41 @@ class Test_Get_By_Consumer_And_Producer:
         response = client.get("/review_for_consumer", params={"consumer_id": consumer_id, "producer_id": producer_id})
         assert response.status_code == 422
 
-    # Invalid object id
+    # Testing with an invalid object id
     def test_get_by_consumer_id_and_producer_id_invalid_ids(self):
         response = client.get("/review_for_consumer", params={"consumer_id": INVALID_OBJECT_ID, "producer_id": INVALID_OBJECT_ID})
+        assert response.json()["detail"] == INVALID_OBJECT_ID + " consumer_id is not a valid ObjectId type!"
         assert response.status_code == 400
-        assert raises(HTTPException)
 
-
-RATING = 2
-TITLE = "This is a test for posting a review for consumer"
-DESCRIPTION = "This is the test description"
 class Test_Post_Review_For_Consumer:
-
     # This fixture will delete reviews that have matching test fields after all the tests in this class have finished. 
-    # Eg RATING = 2, TITLE = "This is a test for posting a review for consumer" ,DESCRIPTION = "This is the test description"
+    # Eg TITLE = "This is a test for posting a review for consumer" ,DESCRIPTION = "This is the test description"
     @fixture(autouse=True)
     def delete_post(self):
         yield
-        review_for_consumer_collection.delete_many({"title": TITLE, "description": DESCRIPTION})
+        review_for_consumer_collection.delete_many({"title": POST_TITLE, "description": POST_DESCRIPTION})
 
     # Post with all fields passed
     def test_post_valid_all_fields(self, get_posted_consumer_id, get_posted_producer_id):
-        payload = {"consumer_id": get_posted_consumer_id, "producer_id": get_posted_producer_id, "rating": RATING, "title": TITLE, "description": DESCRIPTION}
+        payload = {"consumer_id": get_posted_consumer_id, "producer_id": get_posted_producer_id, "rating": POST_RATING, "title": POST_TITLE, "description": POST_DESCRIPTION}
         response = client.post("review_for_consumer/", json=payload)
 
         response_status_code = response.status_code
         response = review_for_consumer_response(**(response.json()))
         response = response.dict()
 
-        assert response_status_code == 201
         assert response["producer_id"] == get_posted_producer_id
         assert response["consumer_id"] == get_posted_consumer_id
-        assert response["rating"] == RATING
-        assert response["title"] == TITLE
-        assert response["description"] == DESCRIPTION
+        assert response["rating"] == POST_RATING
+        assert response["title"] == POST_TITLE
+        assert response["description"] == POST_DESCRIPTION
+        assert response_status_code == 201
     
-    
+    # Test with not sending the required fields
     @mark.parametrize("rating, title, description", [
-        ("", TITLE, DESCRIPTION),
-        (RATING, "", DESCRIPTION),
-        (RATING, TITLE, "")
+        ("", POST_TITLE, POST_DESCRIPTION),
+        (POST_RATING, "", POST_DESCRIPTION),
+        (POST_RATING, POST_TITLE, "")
     ])
     def test_post_missing_fields(self, rating, title, description, get_posted_consumer_id, get_posted_producer_id):
         payload = {"consumer_id": get_posted_consumer_id, "producer_id": get_posted_producer_id, "rating": rating, "title": title, "description": description}
@@ -174,7 +173,7 @@ class Test_Post_Review_For_Consumer:
         (lazy_fixture('get_posted_consumer_id'), "consumer_id")
     ])
     def test_post_only_one_id(self, id, producer_or_consumer):
-        payload = {producer_or_consumer: id, "rating": RATING, "title": TITLE, "description": DESCRIPTION}
+        payload = {producer_or_consumer: id, "rating": POST_RATING, "title": POST_TITLE, "description": POST_DESCRIPTION}
         response = client.post("review_for_consumer/", json=payload)
         assert response.status_code == 422
 
@@ -184,28 +183,28 @@ class Test_Post_Review_For_Consumer:
         (lazy_fixture('get_posted_producer_id'), INVALID_OBJECT_ID)
     ])
     def test_post_invalid_id(self, producer_id, consumer_id):
-        payload = {"producer_id": producer_id, "consumer_id": consumer_id, "rating": RATING, "title": TITLE, "description": DESCRIPTION}
+        payload = {"producer_id": producer_id, "consumer_id": consumer_id, "rating": POST_RATING, "title": POST_TITLE, "description": POST_DESCRIPTION}
         response = client.post("/review_for_consumer/", json=payload)
         assert response.status_code == 400
 
-    # Post with empty id's
+    # Test with empty id's
     @mark.parametrize("producer_id, consumer_id",[
         (lazy_fixture('get_posted_producer_id'), ""),
         ("", lazy_fixture('get_posted_consumer_id')),
         ("", "")
     ])
     def test_post_empty_ids(self, producer_id, consumer_id):
-        payload = {"consumer_id": consumer_id, "producer_id": producer_id, "rating": RATING, "title": TITLE, "description": DESCRIPTION}
+        payload = {"consumer_id": consumer_id, "producer_id": producer_id, "rating": POST_RATING, "title": POST_TITLE, "description": POST_DESCRIPTION}
         response = client.post("review_for_consumer/", json=payload)
         assert response.status_code == 422
 
+    # Test with same id for consumer and producer
     @mark.parametrize("producer_id, consumer_id", [
         (lazy_fixture('get_posted_producer_id'), lazy_fixture('get_posted_producer_id')),
         (lazy_fixture('get_posted_consumer_id'), lazy_fixture('get_posted_consumer_id'))
     ])
-    # Post with id's that aren't in any documents
     def test_post_no_document_with_matching_id(self, producer_id, consumer_id):
-        payload = {"consumer_id": consumer_id, "producer_id": producer_id, "rating": RATING, "title": TITLE, "description": DESCRIPTION}
+        payload = {"consumer_id": consumer_id, "producer_id": producer_id, "rating": POST_RATING, "title": POST_TITLE, "description": POST_DESCRIPTION}
         response = client.post("/review_for_consumer/", json=payload)
         assert response.status_code == 404
         
@@ -216,7 +215,7 @@ class Test_Put_Review_For_Consumer:
         return {"rating": "5", "title": "New updated review", "description": "New updated description"}
 
     # Successful update
-    def test_put(self, get_posted_review):
+    def test_successful_put(self, get_posted_review):
         new_rating = 5
         new_title = "This a update post sent from Test_Put_Review_For_Consumer"
         new_description = "This is the updated description"
@@ -224,13 +223,12 @@ class Test_Put_Review_For_Consumer:
         updated_payload = {"rating": new_rating, "title": new_title, "description": new_description}
         response = client.put("/review_for_consumer/" + get_posted_review["id"], json=updated_payload)
         
+        response_model = review_for_consumer_response(**(response.json())).dict()
+
+        assert response_model["rating"] == new_rating
+        assert response_model["title"] == new_title
+        assert response_model["description"] == new_description
         assert response.status_code == 200
-        
-        response = review_for_consumer_response(**(response.json()))
-        response = response.dict()
-        assert response["rating"] == new_rating
-        assert response["title"] == new_title
-        assert response["description"] == new_description
 
     # Invald id
     def test_put_invalid_id(self, get_payload):
